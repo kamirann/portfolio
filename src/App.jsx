f55b8b08ec196f9cceffd35d57159d7566a1a406
amirtabatabaei69 <45861382+amirtabatabaei69@users.noreply.github.com>
2021-07-29 20:22:28 +0430

    work section has been  created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
 import { Globaly } from "./GlobalVariable";
+import Work from "./components/work/Work";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Globaly />
       <Hero />
       <About />
-      <div style={{ height: "100vh" }}></div>
+      <Work />
     </>
   );
 };
---
+++ src/components/work/WorkItem.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from "react";
+import {
+  Wrapper,
+  Image,
+  Content,
+  ContentH,
+  ContentP,
+  ContentLink,
+} from "./WorkStyles";
+import img from "./img/alaki.png";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/src/all";
+
+const WorkItem = (props) => {
+  gsap.registerPlugin(ScrollTrigger);
+  let itemRef = useRef(null);
+  let containerRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(itemRef.current, {
+      duration: 1,
+      x: "0",
+      scrollTrigger: {
+        trigger: itemRef.current,
+
+        toggleActions: "restart pause reverse pause",
+      },
+    });
+  }, []);
+
+  return (
+    <div ref={containerRef}>
+      <Wrapper ref={itemRef} even={props.even}>
+        <Content even={props.even}>
+          <ContentH even={props.even}>{props.sourceName}</ContentH>
+          <ContentP>{props.subTitle}</ContentP>
+          <ContentLink even={props.even} href={props.urlWork}>
+            Live Preview
+          </ContentLink>
+        </Content>
+        <Image background={img} />
+      </Wrapper>
+    </div>
+  );
+};
+
+export default WorkItem;
